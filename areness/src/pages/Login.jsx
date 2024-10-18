e65f580187e2a799e9cf8bf5793bbc6d554ab936
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-18 14:54:10 +0530

    signup and login

--- areness/src/pages/Login.jsx
+++ areness/src/pages/Login.jsx
@@ -1,11 +1,146 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Image,
+  Checkbox,
+  Button,
+  VStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+} from "@chakra-ui/react";
+import { FaUserPen } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoMdUnlock } from "react-icons/io";
+import { IoLockOpenOutline } from "react-icons/io5";
+import { BsPerson } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <>
+      <Container
+        bg="#FF7373"
+        maxW="full"
+        mt={0}
+        centerContent
+        overflow="hidden"
+      >
+        <Flex>
+          <Box
+            bg="white"
+            color="white"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}
+          >
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem width="50%">
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <Heading color="#FF7373" mb={2}>
+                        Sign in
+                      </Heading>
+                      <VStack spacing={5}>
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <BsPerson color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Username Name"
+                            />
+                          </InputGroup>
+                        </FormControl>
 
-export default Login
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <IoMdUnlock color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Password"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl>
+                          <Checkbox>Remember Me</Checkbox>
+                        </FormControl>
+
+                        <FormControl id="name" float="right">
+                          <Button
+                            variant="solid"
+                            bg="#FF7373"
+                            color="white"
+                            _hover={{}}
+                          >
+                            Login
+                          </Button>
+                        </FormControl>
+
+                        <Flex>
+                          <Text>Or, Login with </Text>
+                          <Image
+                            width="40px"
+                            src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"
+                            alt="facebook"
+                          />
+                          <Image
+                            width="40px"
+                            src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+                            alt="google"
+                          />
+                          <Image
+                            width="40px"
+                            src="https://img.freepik.com/free-vector/new-2023-twitter-logo-x-icon-design_1017-45418.jpg?size=338&ext=jpg&ga=GA1.1.2113030492.1729123200&semt=ais_hybrid"
+                            alt="X"
+                          />
+                        </Flex>
+
+                        <Flex>
+                          <Text>Don't have an account? </Text>
+                          <Link to="/">
+                            <Text color={"blue"}>Create One</Text>
+                          </Link>
+                        </Flex>
+                      </VStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+
+                <WrapItem width="40%">
+                  {/* <Box> */}
+                  <Image
+                    display={{
+                      base: "none",
+                      sm: "none",
+                      md: "block",
+                      lg: "block",
+                    }}
+                    src="https://cdn3d.iconscout.com/3d/premium/preview/man-holding-login-form-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--id-password-signin-ecommerce-and-shopping-pack-e-commerce-illustrations-2426390.png?f=webp&h=700"
+                    alt="image"
+                  />
+                </WrapItem>
+              </Wrap>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
--- areness/src/pages/Signup.jsx
+++ areness/src/pages/Signup.jsx
@@ -1,11 +1,193 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Image,
+  Checkbox,
+  Button,
+  VStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text
+} from "@chakra-ui/react";
+import { FaUserPen } from "react-icons/fa6";
+import {
+  MdOutlineEmail,
+} from "react-icons/md";
+import { IoMdUnlock } from "react-icons/io";
+import { IoLockOpenOutline } from "react-icons/io5";
+import {BsPerson } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      Signup
-    </div>
-  )
-}
+    <>
+      <Container
+        bg="#FF7373"
+        maxW="full"
+        mt={0}
+        centerContent
+        overflow="hidden"
+      >
+        <Flex>
+          <Box
+            bg="white"
+            color="white"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}
+          >
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem width="40%">
+                  {/* <Box> */}
+                  <Image
+                    display={{
+                      base: "none",
+                      sm: "none",
+                      md: "block",
+                      lg: "block",
+                    }}
+                    src="https://thumbs.dreamstime.com/b/registration-form-cute-cartoon-man-sign-up-filling-all-fields-signing-cta-concept-clipboard-to-fill-flat-line-vector-225465986.jpg"
+                    alt="image"
+                  />
+                </WrapItem>
+                <WrapItem width="50%">
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <Heading color="#FF7373" mb={2}>
+                        Signup
+                      </Heading>
+                      <VStack spacing={5}>
+                        <FormControl id="name">
+                          <InputGroup
+                            borderColor="#E0E1E7"
+                            width={{
+                              lg: "400px",
+                              md: "100%",
+                              sm: "100%",
+                              base: "100%",
+                            }}
+                          >
+                            <InputLeftElement pointerEvents="none">
+                              <FaUserPen color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter First Name"
+                            />
+                          </InputGroup>
+                        </FormControl>
 
-export default Signup
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <FaUserPen color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Last Name"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <BsPerson color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Username Name"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <MdOutlineEmail color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Email"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <IoMdUnlock color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Enter Password"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl id="name">
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement pointerEvents="none">
+                              <IoLockOpenOutline color="gray.800" />
+                            </InputLeftElement>
+                            <Input
+                              type="text"
+                              size="md"
+                              placeholder="Confirm Password"
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl>
+                          <Checkbox >I agree to all terms</Checkbox>
+                        </FormControl>
+
+                        <FormControl id="name" float="right">
+                          <Button
+                            variant="solid"
+                            bg="#FF7373"
+                            color="white"
+                            _hover={{}}
+                          >
+                            Signup
+                          </Button>
+
+                          <Flex>
+                          <Text>Already have account? </Text>
+                          <Link to='/login'>
+                             <Text color={'blue'}>Login</Text>
+                            </Link>
+                          </Flex>
+                         
+                        </FormControl>
+
+
+
+
+                      </VStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+              </Wrap>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Signup;
